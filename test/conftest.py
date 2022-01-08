9766d9d4ac0713f345796a95feb6f8262f2ba12b
Hernán de la Fuente <hernan.de@mercadolibre.com>
2022-01-07 21:57:56 -0300

    avances agregando migraciones a los tests, sigue fallando por no reconocer automaticamente la base test

--- test/conftest.py
+++ test/conftest.py
@@ -3,9 +3,19 @@ import pytest
 from src.app import app
 
 
+@pytest.fixture(autouse=True)
+def run_around_test():
+    from src import migrate, rollback
+
+    migrate()
+    yield
+    rollback()
+
+
 @pytest.fixture(scope="session")
 def a_client():
     app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
     yield app.test_client()
 
 
--- test/controller/geometry_controller_test.py
+++ test/controller/geometry_controller_test.py
@@ -1,6 +1,29 @@
-def test_add_new_geometry(a_client, a_geometry_file):
-    data = {"form": {"description": "Una descrip"}, "files": {"file": a_geometry_file}}
+import io
+
+
+def test_add_new_geometry_fails_on_empty_description(a_client, a_geometry_file):
+    data = {
+        "form": {"description": None},
+        "file": (io.BytesIO(a_geometry_file), "name.g01"),
+        "files": {"file": (io.BytesIO(a_geometry_file), "name.g01")},
+    }
 
     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
 
-    assert b"creada con &eacute;xito." in response.data
+    assert b"Error: Ingrese una descripcion" in response.data
+
+
+# def test_add_new_geometry_fails_on_empty_file(a_client, a_geometry_file):
+#     data = {"form": {"description": "Una descrip"}, "files": {"file": a_geometry_file}}
+#
+#     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
+#
+#     assert b'Error: Seleccione un archivo' in response.data
+#
+#
+# def test_add_new_geometry(a_client, a_geometry_file):
+#     data = {"form": {"description": "Una descrip"}, "files": {"file": a_geometry_file}}
+#
+#     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
+#
+#     assert '201' in response.status
