1cd823142bd5b718c84c1f77fb913923b7a9b755
Hernán de la Fuente <hernan.de@mercadolibre.com>
2022-01-10 20:06:01 -0300

    more tests and assertions

--- test/controller/geometry_controller_test.py
+++ test/controller/geometry_controller_test.py
@@ -1,4 +1,8 @@
 import io
+import json
+from datetime import datetime
+
+from src.service import geometry_service
 
 
 def test_add_new_geometry_fails_on_empty_description(a_client, a_geometry_file):
@@ -11,6 +15,7 @@ def test_add_new_geometry_fails_on_empty_description(a_client, a_geometry_file):
     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
 
     assert b"Error: Ingrese una descripc" in response.data
+    assert geometry_service.get_geometry(2) is None
 
 
 def test_add_new_geometry_fails_on_too_long_description(a_client, a_geometry_file):
@@ -26,6 +31,7 @@ def test_add_new_geometry_fails_on_too_long_description(a_client, a_geometry_fil
     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
 
     assert b"entre 1 y 256 caracteres" in response.data
+    assert geometry_service.get_geometry(2) is None
 
 
 def test_add_new_geometry_fails_on_empty_file(a_client, a_geometry_file):
@@ -36,15 +42,80 @@ def test_add_new_geometry_fails_on_empty_file(a_client, a_geometry_file):
 
     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
     assert b"Error: Seleccione un archivo" in response.data
+    assert geometry_service.get_geometry(2) is None
 
 
 def test_add_new_geometry_success(a_client, a_geometry_file):
+    filename = "test_geometry.g01"
+    description = "some_description"
     data = {
-        "description": "some_description",
-        "file": (io.BytesIO(a_geometry_file), "test_geometry.g01"),
-        "files": {"file": (io.BytesIO(a_geometry_file), "test_geometry.g01")},
+        "description": description,
+        "file": (io.BytesIO(a_geometry_file), filename),
+        "files": {"file": (io.BytesIO(a_geometry_file), filename)},
     }
 
     response = a_client.post("/geometry", data=data, content_type="multipart/form-data")
 
     assert b"creada con \xc3\xa9xito." in response.data
+    geometry = geometry_service.get_geometry(2)
+    assert geometry.name == filename
+    assert geometry.description == description
+    assert geometry.user_id == 1
+
+
+def test_file_name_secure_on_geometry_creation(a_client, a_geometry_file):
+    data = {
+        "description": "some_description",
+        "file": (io.BytesIO(a_geometry_file), "test geometry.g01"),
+        "files": {"file": (io.BytesIO(a_geometry_file), "test geometry.g01")},
+    }
+
+    a_client.post("/geometry", data=data, content_type="multipart/form-data")
+
+    geometry = geometry_service.get_geometry(2)
+    assert geometry.name == "test_geometry.g01"
+
+
+def test_list_geometries_when_there_only_one(a_client):
+    response = a_client.get("/geometry")
+    expected_response = {
+        "rows": [
+            {
+                "created_at": "21/12/2021",
+                "description": "Ejemplo dado por el INA",
+                "id": 1,
+                "name": "Modelo1-Atucha",
+                "user": "Admin Ina",
+            }
+        ],
+        "total": 1,
+    }
+
+    assert json.loads(response.data) == expected_response
+
+
+def test_list_geometries_when_there_two(a_client, a_geometry_file):
+    test_add_new_geometry_success(a_client, a_geometry_file)
+
+    response = a_client.get("/geometry")
+    expected_response = {
+        "rows": [
+            {
+                "created_at": "21/12/2021",
+                "description": "Ejemplo dado por el INA",
+                "id": 1,
+                "name": "Modelo1-Atucha",
+                "user": "Admin Ina",
+            },
+            {
+                "created_at": datetime.now().strftime("%d/%m/%Y"),
+                "description": "some_description",
+                "id": 2,
+                "name": "test_geometry.g01",
+                "user": "Admin Ina",
+            },
+        ],
+        "total": 2,
+    }
+
+    assert json.loads(response.data) == expected_response
