286c867c06d83261c8e0abdea91d90a87203cc9f
Mariano Hielpos <marianohielpos@gmail.com>
2022-04-18 10:31:40 -0300

    linter

--- src/parana/utils.py
+++ src/parana/utils.py
@@ -7,46 +7,52 @@ def delete_jumps_parana_santa_fe_diamante(df_base):
 
     # Parana
     for index, row in df_base.iterrows():
-        if abs(row['Parana_diff']) > umbral_1:
-            if (abs(row['SantaFe_diff']) > umbral_1) and (abs(row['Diamante_diff']) > umbral_1):
-                if abs(row['Parana_diff']) > umbral_2:
-                    df_base.loc[index, 'Parana'] = np.nan
+        if abs(row["Parana_diff"]) > umbral_1:
+            if (abs(row["SantaFe_diff"]) > umbral_1) and (
+                abs(row["Diamante_diff"]) > umbral_1
+            ):
+                if abs(row["Parana_diff"]) > umbral_2:
+                    df_base.loc[index, "Parana"] = np.nan
                 else:
                     # print('Los 3 presentan un salto. Se supone que esta ok.')
                     continue
             else:
                 # print('Salto en Parana y StFe o Diamante')
-                df_base.loc[index, 'Parana'] = np.nan
+                df_base.loc[index, "Parana"] = np.nan
         else:
             continue
 
     # Santa Fe
     for index, row in df_base.iterrows():
-        if abs(row['SantaFe_diff']) > umbral_1:
-            if (abs(row['Parana_diff']) > umbral_1) and (abs(row['Diamante_diff']) > umbral_1):
-                if abs(row['SantaFe_diff']) > umbral_2:
-                    df_base.loc[index, 'SantaFe'] = np.nan
+        if abs(row["SantaFe_diff"]) > umbral_1:
+            if (abs(row["Parana_diff"]) > umbral_1) and (
+                abs(row["Diamante_diff"]) > umbral_1
+            ):
+                if abs(row["SantaFe_diff"]) > umbral_2:
+                    df_base.loc[index, "SantaFe"] = np.nan
                 else:
                     # print('Los 3 presentan un salto. Se supone que esta ok.')
                     continue
             else:
                 # print('Salto en StFe y Parana o Diamante')
-                df_base.loc[index, 'SantaFe'] = np.nan
+                df_base.loc[index, "SantaFe"] = np.nan
         else:
             continue
 
     # Diamante
     for index, row in df_base.iterrows():
-        if abs(row['Diamante_diff']) > umbral_1:
-            if (abs(row['Parana_diff']) > umbral_1) and (abs(row['SantaFe_diff']) > umbral_1):
-                if abs(row['Diamante_diff']) > umbral_2:
-                    df_base.loc[index, 'Diamante'] = np.nan
+        if abs(row["Diamante_diff"]) > umbral_1:
+            if (abs(row["Parana_diff"]) > umbral_1) and (
+                abs(row["SantaFe_diff"]) > umbral_1
+            ):
+                if abs(row["Diamante_diff"]) > umbral_2:
+                    df_base.loc[index, "Diamante"] = np.nan
                 else:
                     # print('Los 3 presentan un salto. Se supone que esta ok.')
                     continue
             else:
                 # print('Salto en Diamante y Parana o StFe')
-                df_base.loc[index, 'Diamante'] = np.nan
+                df_base.loc[index, "Diamante"] = np.nan
         else:
             continue
     return df_base
@@ -58,30 +64,30 @@ def delete_jumps_san_fernando_bs_as(df_base):  # Elimina Saltos en la serie
 
     # SFernando
     for index, row in df_base.iterrows():
-        if abs(row['SanFernando_diff']) > umbral_1:
-            if abs(row['BsAs_diff']) > umbral_1:
-                if abs(row['SanFernando_diff']) > umbral_2:
-                    df_base.loc[index, 'SanFernando'] = np.nan
+        if abs(row["SanFernando_diff"]) > umbral_1:
+            if abs(row["BsAs_diff"]) > umbral_1:
+                if abs(row["SanFernando_diff"]) > umbral_2:
+                    df_base.loc[index, "SanFernando"] = np.nan
                 else:
                     continue
                     # print('Los 3 presentan un salto. Se supone que esta ok.')
             else:
                 # print('Salto en SanFer y BsAs o Brag')
-                df_base.loc[index, 'SanFernando'] = np.nan
+                df_base.loc[index, "SanFernando"] = np.nan
         else:
             continue
     # BsAs
     for index, row in df_base.iterrows():
-        if abs(row['BsAs_diff']) > umbral_1:
-            if abs(row['SanFernando_diff']) > umbral_1:
-                if abs(row['BsAs_diff']) > umbral_2:
-                    df_base.loc[index, 'BsAs'] = np.nan
+        if abs(row["BsAs_diff"]) > umbral_1:
+            if abs(row["SanFernando_diff"]) > umbral_1:
+                if abs(row["BsAs_diff"]) > umbral_2:
+                    df_base.loc[index, "BsAs"] = np.nan
                 else:
                     continue
                     # print('Los 3 presentan un salto. Se supone que esta ok.')
             else:
                 # print('Salto en BsAs y SFer o Braga')
-                df_base.loc[index, 'BsAs'] = np.nan
+                df_base.loc[index, "BsAs"] = np.nan
         else:
             continue
     return df_base
@@ -93,31 +99,31 @@ def delete_jumps_nueva_palmira_martinez(df_base):  # Elimina Saltos en la serie
 
     # Nueva Palmira
     for index, row in df_base.iterrows():
-        if abs(row['Nueva Palmira_diff']) > umbral_1:
-            if abs(row['Martinez_diff']) > umbral_1:
-                if abs(row['Nueva Palmira_diff']) > umbral_2:
-                    df_base.loc[index, 'Nueva Palmira'] = np.nan
+        if abs(row["Nueva Palmira_diff"]) > umbral_1:
+            if abs(row["Martinez_diff"]) > umbral_1:
+                if abs(row["Nueva Palmira_diff"]) > umbral_2:
+                    df_base.loc[index, "Nueva Palmira"] = np.nan
                 else:
                     continue
                     # print('Los 2 presentan un salto. Se supone que esta ok.')
             else:
                 # print('Salto en NPalmira')
-                df_base.loc[index, 'Nueva Palmira'] = np.nan
+                df_base.loc[index, "Nueva Palmira"] = np.nan
         else:
             continue
 
     # Martinez
     for index, row in df_base.iterrows():
-        if abs(row['Martinez_diff']) > umbral_1:
-            if abs(row['Nueva Palmira_diff']) > umbral_1:
-                if abs(row['Martinez_diff']) > umbral_2:
-                    df_base.loc[index, 'Martinez'] = np.nan
+        if abs(row["Martinez_diff"]) > umbral_1:
+            if abs(row["Nueva Palmira_diff"]) > umbral_1:
+                if abs(row["Martinez_diff"]) > umbral_2:
+                    df_base.loc[index, "Martinez"] = np.nan
                 else:
                     continue
                     # print('Los 2 presentan un salto. Se supone que esta ok.')
             else:
                 # print('Salto en Martinez')
-                df_base.loc[index, 'Martinez'] = np.nan
+                df_base.loc[index, "Martinez"] = np.nan
         else:
             continue
 
@@ -128,5 +134,5 @@ def complete_missings(df_ConMedia):
     ncol = df_ConMedia.columns[0]
     for index, row in df_ConMedia.iterrows():
         if np.isnan(row[ncol]):
-            df_ConMedia.loc[index, ncol] = row['medio']
+            df_ConMedia.loc[index, ncol] = row["medio"]
     return df_ConMedia
--- src/service/ina_service.py
+++ src/service/ina_service.py
@@ -6,9 +6,11 @@ from datetime import timedelta
 def obtain_obeservations_for_stations(stations, timestart, timeend):
     dfs = []
     for index, row in stations.T.iteritems():
-        id_serie = row['series_id']
-        df_obs = obtain_observations(id_serie, timestart.strftime("%Y-%m-%d"), timeend.strftime("%Y-%m-%d"))
-        df_obs['id'] = index
+        id_serie = row["series_id"]
+        df_obs = obtain_observations(
+            id_serie, timestart.strftime("%Y-%m-%d"), timeend.strftime("%Y-%m-%d")
+        )
+        df_obs["id"] = index
         dfs.append(df_obs)
 
     return pd.concat(dfs, ignore_index=True)
@@ -16,22 +18,26 @@ def obtain_obeservations_for_stations(stations, timestart, timeend):
 
 def obtain_observations(serie_id, timestart, timeend):
     response = requests.get(
-        'https://alerta.ina.gob.ar/a5/obs/puntual/series/' + str(serie_id) + '/observaciones',
-        params={'timestart': timestart, 'timeend': timeend},
+        "https://alerta.ina.gob.ar/a5/obs/puntual/series/"
+        + str(serie_id)
+        + "/observaciones",
+        params={"timestart": timestart, "timeend": timeend},
         headers={
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlNhbnRpYWdvIEd1aXp6YXJkaSIsImlhdCI6MTUxNjIzOTAyMn0.YjqQYMCh4AIKSsSEq-QsTGz3Q4WOS5VE-CplGQdInfQ'}, )
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlNhbnRpYWdvIEd1aXp6YXJkaSIsImlhdCI6MTUxNjIzOTAyMn0.YjqQYMCh4AIKSsSEq-QsTGz3Q4WOS5VE-CplGQdInfQ"
+        },
+    )
     json_response = response.json()
 
-    df_obs_i = pd.DataFrame.from_dict(json_response, orient='columns')
-    df_obs_i = df_obs_i[['timestart', 'valor']]
-    df_obs_i = df_obs_i.rename(columns={'timestart': 'fecha'})
+    df_obs_i = pd.DataFrame.from_dict(json_response, orient="columns")
+    df_obs_i = df_obs_i[["timestart", "valor"]]
+    df_obs_i = df_obs_i.rename(columns={"timestart": "fecha"})
 
-    df_obs_i['fecha'] = pd.to_datetime(df_obs_i['fecha'])
-    df_obs_i['valor'] = df_obs_i['valor'].astype(float)
+    df_obs_i["fecha"] = pd.to_datetime(df_obs_i["fecha"])
+    df_obs_i["valor"] = df_obs_i["valor"].astype(float)
 
-    df_obs_i = df_obs_i.sort_values(by='fecha')
+    df_obs_i = df_obs_i.sort_values(by="fecha")
 
-    df_obs_i['fecha'] = df_obs_i.fecha.dt.tz_convert(None)
-    df_obs_i['fecha'] = df_obs_i.fecha - timedelta(hours=3)
+    df_obs_i["fecha"] = df_obs_i.fecha.dt.tz_convert(None)
+    df_obs_i["fecha"] = df_obs_i.fecha - timedelta(hours=3)
 
     return df_obs_i
