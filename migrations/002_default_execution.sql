22877eb2f22d612b8993afa35d4fc6a1b955bdd0
Santiago Dorgan <santidorgan@gmail.com>
2022-07-23 19:33:10 -0300

    Fix sql

--- migrations/002_default_execution.sql
+++ migrations/002_default_execution.sql
@@ -5,7 +5,7 @@ INSERT INTO gesina.geometry (id, "name", description, created_at, user_id) VALUE
 	 (1, 'Modelo1-Atucha.g01', 'Ejemplo dado por el INA','2021-12-21 20:08:36.377642', 1);
 
 INSERT INTO gesina.geometry (id, "name", description, created_at, user_id) VALUES
-	 (,2 'DeltaParana_2017.g23', 'Geometría Paraná','NOW()', 1);
+	 (2, 'DeltaParana_2017.g23', 'Geometría Paraná','NOW()', 1);
 
 INSERT INTO gesina.execution_plan (plan_name,geometry_id, user_id, start_datetime, end_datetime, created_at, status) VALUES
 	 ('ejemplo-ina', 1, 1, '2021-12-21 20:08:39.133122', '2021-12-21 20:08:39.133122', '2021-12-21 20:08:39.133122', 'PENDING');
