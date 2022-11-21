43ed87ddc019c05fccbc3a6736a9852001e6c6fc
Mariano Hielpos <marianohielpos@gmail.com>
2022-11-21 11:21:47 -0300

    quito info

--- installation/run_in_windows.ps1
+++ installation/run_in_windows.ps1
@@ -1,10 +1,6 @@
 cd C:\gesina-core-api\
 pipenv shell
 cd C:\gesina-core-api\
-$Env:INA_TOKEN='INA_TOKEN'
-$Env:INA_URL='https://alerta.ina.gob.ar/a6'
-$Env:INA_TOKEN_ENVIO='INA_TOKEN_ENVIO'
-$Env:INA_URL_ENVIO='https://alerta.ina.gob.ar/test'
 $Env:BROKER_URL='redis://10.0.2.2:6379/0'
 $Env:RESULT_BACKEND='redis://10.0.2.2:6379/0'
 $Env:DATABASE_HOST='10.0.2.2:5432'
