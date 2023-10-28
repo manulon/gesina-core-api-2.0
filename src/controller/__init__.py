578c3d7c6935be97b7e1fc92b4f941f957bd7a51
Federico Burman <fburman@fi.uba.ar>
2023-10-28 18:44:17 -0300

    Add rest api skeleton

--- src/controller/__init__.py
+++ src/controller/__init__.py
@@ -4,3 +4,4 @@ from src.controller.public_view_controller import PUBLIC_VIEW_BLUEPRINT
 from src.controller.execution_plan_controller import EXECUTION_PLAN_BLUEPRINT
 from src.controller.schedule_controller import SCHEDULE_TASK_BLUEPRINT
 from src.controller.user_controller import USER_BLUEPRINT
+from src.controller.api_controller import API_BLUEPRINT
