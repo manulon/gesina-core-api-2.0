566221f681acbfd95b44b33292a227e9666cc8b7
azulzaietz <azulzaietz@gmail.com>
2023-11-25 17:55:30 -0300

    columna en init sql

--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -46,6 +46,7 @@ create table if not exists execution_plan
 	start_datetime timestamp not null,
 	end_datetime timestamp not null,
 	created_at timestamp not null,
+    scheduled_task_id integer,
 	status text not null
 );
 
