1846d615ac22a75339486d42d00ae57e14dd27e4
azulzaietz <azulzaietz@gmail.com>
2024-05-01 17:32:22 -0300

    [FIX] Geometry API

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ secret_key = os.getenv("SECRET_KEY", "default")
 # Application
 dry_run = os.getenv("DRY_RUN", False)
 fake_activity = os.getenv("FAKE_ACTIVITY", False)
-ina_token = os.getenv("INA_TOKEN", "")
+ina_token = os.getenv("INA_TOKEN", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6Imdlc2luYTIiLCJpYXQiOjE1MTYyMzkwMjJ9.2fbGNi4u-3opg7VnnxGgcqS2fVWu-66a6ird7cv-IKk")
 ina_url = os.getenv("INA_URL", "https://alerta.ina.gob.ar/a6")
 ina_token_envio = os.getenv("INA_TOKEN_ENVIO", "")
 ina_url_envio = os.getenv("INA_URL_ENVIO", "https://alerta.ina.gob.ar/test")
